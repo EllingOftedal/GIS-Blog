2f5bd6327dc697a430e9dab28556ddf8f3588dba
EllingOftedal <127117872+EllingOftedal@users.noreply.github.com>
2023-04-14 16:24:52 +0200

    Create leaflet-heatmap.js

---
+++ scripts/js/leaflet-heatmap.js
@@ -0,0 +1,21 @@
+Papa.parse('scripts/nrk/global/results/countries_output.csv', {
+  download: true,
+  header: true,
+  complete: function (results) {
+    const data = results.data;
+    data.forEach(function (row) {
+      heatLayer.addLatLng([row.Latitude, row.Longitude]);
+    });
+  }
+});
+
+Papa.parse('scripts/nrk/inland/results/innland_output.csv', {
+  download: true,
+  header: true,
+  complete: function (results) {
+    const data = results.data;
+    data.forEach(function (row) {
+      heatLayer.addLatLng([row.latitude, row.longitude]);
+    });
+  }
+});
