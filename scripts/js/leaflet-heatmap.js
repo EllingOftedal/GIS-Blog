02b4c92f35289bc1f92698a87e711a3df96bb464
EllingOftedal <127117872+EllingOftedal@users.noreply.github.com>
2023-04-15 10:04:20 +0200

    Update leaflet-heatmap.js

--- scripts/js/leaflet-heatmap.js
+++ scripts/js/leaflet-heatmap.js
@@ -3,6 +3,7 @@ Papa.parse('../scripts/nrk/global/results/countries_output.csv', {
   header: true,
   complete: function (results) {
     const data = results.data;
+    console.log('Global data:', data);
     data.forEach(function (row) {
       if (row.Latitude && row.Longitude) {
         globalHeatLayer.addLatLng([row.Latitude, row.Longitude]);
@@ -18,6 +19,7 @@ Papa.parse('../scripts/nrk/inland/results/innland_output.csv', {
   header: true,
   complete: function (results) {
     const data = results.data;
+    console.log('Local data:', data);
     data.forEach(function (row) {
       if (row.latitude && row.longitude) {
         localHeatLayer.addLatLng([row.latitude, row.longitude]);
