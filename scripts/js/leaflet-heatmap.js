5a45353a84d19798c5b5bea9d7288cf4797f4c88
EllingOftedal <127117872+EllingOftedal@users.noreply.github.com>
2023-04-15 10:11:16 +0200

    Update leaflet-heatmap.js

--- scripts/js/leaflet-heatmap.js
+++ scripts/js/leaflet-heatmap.js
@@ -1,31 +1,45 @@
-Papa.parse('../scripts/nrk/global/results/countries_output.csv', {
-  download: true,
-  header: true,
-  complete: function (results) {
-    const data = results.data;
-    console.log('Global data:', data);
-    data.forEach(function (row) {
-      if (row.Latitude && row.Longitude) {
-        globalHeatLayer.addLatLng([row.Latitude, row.Longitude]);
-      } else {
-        console.warn('Invalid data:', row);
-      }
-    });
-  }
-});
+document.addEventListener('DOMContentLoaded', function () {
+  var globalMap = L.map('global-map').setView([0, 0], 2);
+  var globalLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors',
+    maxZoom: 18
+  }).addTo(globalMap);
+  var globalHeatLayer = L.heatLayer([], {radius: 25}).addTo(globalMap);
 
-Papa.parse('../scripts/nrk/inland/results/innland_output.csv', {
-  download: true,
-  header: true,
-  complete: function (results) {
-    const data = results.data;
-    console.log('Local data:', data);
-    data.forEach(function (row) {
-      if (row.latitude && row.longitude) {
-        localHeatLayer.addLatLng([row.latitude, row.longitude]);
-      } else {
-        console.warn('Invalid data:', row);
-      }
-    });
-  }
+  var localMap = L.map('local-map').setView([60.4720, 8.4689], 5);
+  var localLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors',
+    maxZoom: 18
+  }).addTo(localMap);
+  var localHeatLayer = L.heatLayer([], {radius: 25}).addTo(localMap);
+
+  Papa.parse('../scripts/nrk/global/results/countries_output.csv', {
+    download: true,
+    header: true,
+    complete: function (results) {
+      const data = results.data;
+      data.forEach(function (row) {
+        if (row.Latitude && row.Longitude) {
+          globalHeatLayer.addLatLng([row.Latitude, row.Longitude]);
+        } else {
+          console.warn('Invalid data:', row);
+        }
+      });
+    }
+  });
+
+  Papa.parse('../scripts/nrk/inland/results/innland_output.csv', {
+    download: true,
+    header: true,
+    complete: function (results) {
+      const data = results.data;
+      data.forEach(function (row) {
+        if (row.latitude && row.longitude) {
+          localHeatLayer.addLatLng([row.latitude, row.longitude]);
+        } else {
+          console.warn('Invalid data:', row);
+        }
+      });
+    }
+  });
 });
