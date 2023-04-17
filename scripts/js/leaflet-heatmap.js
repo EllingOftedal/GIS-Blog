fddbe2602e4eb380db1741ef05b93d566d0b3dce
EllingOftedal <127117872+EllingOftedal@users.noreply.github.com>
2023-04-17 12:20:21 +0200

    Update leaflet-heatmap.js

--- scripts/js/leaflet-heatmap.js
+++ scripts/js/leaflet-heatmap.js
@@ -55,39 +55,38 @@ function initializeMaps() {
     updateHeatmapRadius(localMap, localHeatLayer);
   });
 
-  Papa.parse('../scripts/nrk/global/results/countries_summarized.csv', {
-    download: true,
-    header: true,
-    complete: function (results) {
-      const data = results.data;
-      const maxCount = getMaxCount(data);
-      data.forEach(function (row) {
-        if (row.latitude && row.longitude && row.count) {
-          const count = parseInt(row.count, 10);
-          addWeightedLatLng(globalHeatLayer, parseFloat(row.latitude), parseFloat(row.longitude), count);
-        } else {
-          console.warn('Invalid data:', row);
-        }
-      });
+Papa.parse('../scripts/nrk/global/results/countries_summarized.csv', {
+  download: true,
+  header: true,
+  complete: function (results) {
+    const data = results.data;
+    const maxCount = getMaxCount(data);
+    data.forEach(function (row) {
+      if (row.latitude && row.longitude && row.count) {
+        const count = parseInt(row.count, 10);
+        addWeightedLatLng(globalHeatLayer, parseFloat(row.latitude), parseFloat(row.longitude), count);
+      } else {
+        console.warn('Invalid data:', row);
+      }
+    });
+  } // Add the missing closing brace here
+});
 
-
-  Papa.parse('../scripts/nrk/inland/results/innland_summarized.csv', {
-    download: true,
-    header: true,
-    complete: function (results) {
-      const data = results.data;
-      const maxCount = getMaxCount(data);
-      data.forEach(function (row) {
-        if (row.latitude && row.longitude && row.count) {
-          const count = parseInt(row.count, 10);
-          addWeightedLatLng(globalHeatLayer, parseFloat(row.latitude), parseFloat(row.longitude), count);
-        } else {
-          console.warn('Invalid data:', row);
-        }
-      });
-
-    }
-  });
-}
+Papa.parse('../scripts/nrk/inland/results/innland_summarized.csv', {
+  download: true,
+  header: true,
+  complete: function (results) {
+    const data = results.data;
+    const maxCount = getMaxCount(data);
+    data.forEach(function (row) {
+      if (row.latitude && row.longitude && row.count) {
+        const count = parseInt(row.count, 10);
+        addWeightedLatLng(globalHeatLayer, parseFloat(row.latitude), parseFloat(row.longitude), count);
+      } else {
+        console.warn('Invalid data:', row);
+      }
+    });
+  }
+});
 
 document.addEventListener('DOMContentLoaded', initializeMaps);
