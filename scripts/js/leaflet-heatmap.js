7d97dc88f5de2fee2e46e9ed5695ce9ce82c0bb5
EllingOftedal <127117872+EllingOftedal@users.noreply.github.com>
2023-04-18 19:30:13 +0200

    Update leaflet-heatmap.js

--- scripts/js/leaflet-heatmap.js
+++ scripts/js/leaflet-heatmap.js
@@ -1,27 +1,5 @@
-function addWeightedLatLng(heatLayer, lat, lng, count) {
-  heatLayer.addLatLng([lat, lng, count]);
-}
-
-function updateHeatmapRadius(map, heatLayer) {
-  const baseRadius = 25;
-  const currentZoom = map.getZoom();
-  const newRadius = baseRadius * currentZoom;
-  heatLayer.setOptions({ radius: newRadius });
-}
-
-function getMaxCount(data) {
-  let maxCount = 0;
-  data.forEach(function (row) {
-    const count = parseInt(row.count, 10);
-    if (count > maxCount) {
-      maxCount = count;
-    }
-  });
-  return maxCount;
-}
-
-function isValidData(row) {
-  return row.latitude && row.longitude && row.count;
+function addLatLng(heatLayer, lat, lng) {
+  heatLayer.addData({ x: lat, y: lng });
 }
 
 function initializeMaps() {
@@ -30,42 +8,40 @@ function initializeMaps() {
     attribution: '&copy; OpenStreetMap contributors',
     maxZoom: 18
   }).addTo(globalMap);
-  var globalHeatLayer = L.heatLayer([], {
-    radius: 25,
-    gradient: {0.0: '#00ccff', 0.5: '#ff9900', 1.0: '#ff0000'},
-    maxOpacity: 0.4,
-  }).addTo(globalMap);
 
   var localMap = L.map('local-map').setView([60.4720, 8.4689], 5);
   var localLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors',
     maxZoom: 18
   }).addTo(localMap);
-  var localHeatLayer = L.heatLayer([], {
-    radius: 25,
-    gradient: {0.0: '#00ccff', 0.5: '#ff9900', 1.0: '#ff0000'},
+
+  var globalHeatLayer = new HeatmapOverlay({
+    radius: 1,
     maxOpacity: 0.4,
+    scaleRadius: true,
+    useLocalExtrema: false,
+    latField: 'x',
+    lngField: 'y'
+  }).addTo(globalMap);
+
+  var localHeatLayer = new HeatmapOverlay({
+    radius: 1,
+    maxOpacity: 0.4,
+    scaleRadius: true,
+    useLocalExtrema: false,
+    latField: 'x',
+    lngField: 'y'
   }).addTo(localMap);
 
-  globalMap.on('zoomend', function () {
-    updateHeatmapRadius(globalMap, globalHeatLayer);
-  });
-
-  localMap.on('zoomend', function () {
-    updateHeatmapRadius(localMap, localHeatLayer);
-  });
-
   // Global data map
-  Papa.parse('../scripts/nrk/global/results/countries_summarized.csv', {
+  Papa.parse('../scripts/nrk/global/results/raw_data.csv', {
     download: true,
     header: true,
     complete: function (results) {
       const data = results.data;
-      const globalMaxCount = getMaxCount(data);
       data.forEach(function (row) {
-        if (isValidData(row)) {
-          const count = parseInt(row.count, 10);
-          addWeightedLatLng(globalHeatLayer, parseFloat(row.latitude), parseFloat(row.longitude), count);
+        if (row.latitude && row.longitude) {
+          addLatLng(globalHeatLayer, parseFloat(row.latitude), parseFloat(row.longitude));
         } else {
           console.warn('Invalid data:', row);
         }
@@ -74,16 +50,14 @@ function initializeMaps() {
   });
 
   // Norway inland data map
-  Papa.parse('../scripts/nrk/inland/results/innland_summarized.csv', {
+  Papa.parse('../scripts/nrk/inland/results/raw_data.csv', {
     download: true,
     header: true,
     complete: function (results) {
       const data = results.data;
-      const inlandMaxCount = getMaxCount(data);
       data.forEach(function (row) {
-        if (isValidData(row)) {
-          const count = parseInt(row.count, 10);
-          addWeightedLatLng(localHeatLayer, parseFloat(row.latitude), parseFloat(row.longitude), count);
+        if (row.latitude && row.longitude) {
+          addLatLng(localHeatLayer, parseFloat(row.latitude), parseFloat(row.longitude));
         } else {
           console.warn('Invalid data:', row);
         }
